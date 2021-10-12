83d5ca54bc85fe6638f4d8c13ada228aa6f37428
UserAlberto <af1pb.albertomarti@gmail.com>
2021-10-12 13:44:08 +0200

    Creado componente footer y directiva, dando estilos y utilizando el iterador *ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectivaComponent } from './components/directiva/directiva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
